73679b44938dd2897e30cd615a9c307cfb52d4ac
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-02-13 10:27:32 +0900

    平泉市一覧表示完成

--- お試し2.py
+++ お試し2.py
@@ -1,7 +1,7 @@
 import requests as requests
 from bs4 import BeautifulSoup
 
-url = "https://tabelog.com/tokyo/A1301/A130103/R5266/rstLst/?vs=1&sa=%E6%96%B0%E6%A9%8B%E9%A7%85&sk=%25E5%2580%258B%25E5%25AE%25A4&lid=hd_search1&vac_net=&svd=20200323&svt=1900&svps=2&hfc=1&ChkRoom=1&cat_sk=%E5%80%8B%E5%AE%A4"
+url = "https://tabelog.com/iwate/A0303/A030303/R8556/rstLst/?vs=1&sa=%E5%B9%B3%E6%B3%89%E9%A7%85&sk=%25E5%2580%258B%25E5%25AE%25A4&lid=hd_search1&vac_net=&svd=20220213&svt=1900&svps=2&hfc==1&ChkSemiRoom=1&cat_sk=%E5%80%8B%E5%AE%A4"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -13,3 +13,5 @@ restrants = soup.find_all("div", class_="list-rst__wrap js-open-new-window")
 
 for r in restrants:
     print(r.find("a").text, r.a.get("href"))
+
+    print(r)
